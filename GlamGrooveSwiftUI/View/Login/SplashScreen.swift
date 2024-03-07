82519bb425f02ff73e63ae6b5374f8c83864e999
shanuka-2242 <shanukapayoe@gmail.com>
2024-03-07 06:37:39 -0800

    MVVM Folder struct created & Added Launch screen

---
+++ GlamGrooveSwiftUI/View/Login/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  GlamGrooveSwiftUI
+//
+//  Created by NIBM-LAB04-PC03 on 2024-03-07.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
