770122868958f5533f7b4affe88279480af6e7f3
shanuka-2242 <shanukapayoe@gmail.com>
2024-03-07 05:17:05 -0800

    Initial Commit

---
+++ GlamGrooveSwiftUI/GlamGrooveSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlamGrooveSwiftUIApp.swift
+//  GlamGrooveSwiftUI
+//
+//  Created by NIBM-LAB04-PC03 on 2024-03-07.
+//
+
+import SwiftUI
+
+@main
+struct GlamGrooveSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
