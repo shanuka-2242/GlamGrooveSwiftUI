d552d96f35eb706db4eda06d5ff9dc71e4440710
shanuka-2242 <shanukapayoe@gmail.com>
2024-03-12 06:37:45 -0700

    Launch screen implemented

--- GlamGrooveSwiftUI/View/Login/WelcomeScreen.swift
+++ GlamGrooveSwiftUI/View/Login/WelcomeScreen.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct SplashScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("WelcomeScreenImage")
+                .resizable()
+                .scaledToFill()
+        }
+        .ignoresSafeArea()
     }
 }
 
