66d8b56e8cff19cf83526861b89e532cbdf9f859
shanuka-2242 <shanukapayoe@gmail.com>
2024-03-12 06:33:28 -0700

    Launch screen created

---
+++ GlamGrooveSwiftUI/UICommon/UIExtention.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtention.swift
+//  GlamGrooveSwiftUI
+//
+//  Created by NIBM-LAB04-PC03 on 2024-03-12.
+//
+
+import SwiftUI
+
+struct UIExtention: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtention()
+}
