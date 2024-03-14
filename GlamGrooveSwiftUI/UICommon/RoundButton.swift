f6879fa839cc8773fe7415810685cf46ef1f26dc
shanuka-2242 <shanukapayoe@gmail.com>
2024-03-14 07:16:10 -0700

    Welcome screen designed

---
+++ GlamGrooveSwiftUI/UICommon/RoundButton.swift
@@ -0,0 +1,18 @@
+//
+//  RoundButton.swift
+//  GlamGrooveSwiftUI
+//
+//  Created by NIBM-LAB04-PC03 on 2024-03-14.
+//
+
+import SwiftUI
+
+struct RoundButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundButton()
+}
