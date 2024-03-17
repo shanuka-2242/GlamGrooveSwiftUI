d0dc048efc1409f0e763c57fa421475845671640
shanuka-2242 <shanukapayoe@gmail.com>
2024-03-17 16:28:02 +0530

    Catagory cell and product cell added

---
+++ GlamGrooveSwiftUI/UICommon/CatagoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  CatagoryCell.swift
+//  GlamGrooveSwiftUI
+//
+//  Created by NIBM-LAB04-PC04 on 2024-03-17.
+//
+
+import SwiftUI
+
+struct CatagoryCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatagoryCell()
+}
---
+++ GlamGrooveSwiftUI/UICommon/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  GlamGrooveSwiftUI
+//
+//  Created by NIBM-LAB04-PC04 on 2024-03-17.
+//
+
+import SwiftUI
+
+struct ProductCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductCell()
+}
