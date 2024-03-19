8357f7d5dc37d300f7c679a329d1a6f1713ebe00
shanuka-2242 <shanukapayoe@gmail.com>
2024-03-19 07:24:06 -0700

    Uncommited changes commited

--- GlamGrooveSwiftUI/Model/ProductModel.swift
+++ GlamGrooveSwiftUI/Model/ProductModel.swift
@@ -7,12 +7,22 @@
 
 import SwiftUI
 
-struct ProductModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct ProductModel: Identifiable, Equatable {
+    
+    var id: Int = 0;
+    //var id: Int = 0;
+    //var id: Int = 0;
+    //var id: Int = 0;
+
 }
 
-#Preview {
-    ProductModel()
-}
+//[
+//  {
+//    "_id": "65f8a684dcce79d70260ec36",
+//    "catagoryName": "Jeans",
+//    "productName": "Sammy Mid Waisted Straight Capri",
+//    "productPrice": "Rs. 3800.00",
+//    "productImage": "Sammy Mid Waisted Straight Capri",
+//    "__v": 0
+//  }
+//]
